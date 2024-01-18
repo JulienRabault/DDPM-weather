FIX leakage GPU mem

main.py
@@ -24,6 +24,7 @@ from utils.distributed import get_rank_num, get_rank, is_main_gpu, synchronize
 
 from itertools import product
 import numpy as np
+from torch.profiler import profile, record_function, ProfilerActivity
 # list of parameters available for grid search
 # every parameters must be modified in config_schema.json
 # with "oneOf" to handle 2 types of variable.
@@ -32,6 +33,7 @@ GRIDSEARCH_PARAM = ["batch_size", "lr", "beta_schedule"]
 warnings.filterwarnings(
     "ignore", message="This DataLoader will create .* worker processes in total.*")
 gc.collect()
+# Free GPU cache
 torch.cuda.empty_cache()
 
 
@@ -137,7 +139,7 @@ def prepare_dataloader(config, path, csv_file):
         num_workers=cpu_count(),
         sampler=DistributedSampler(train_set, rank=get_rank_num(), shuffle=False,
                                    drop_last=False) if torch.cuda.device_count() >= 2 else None,
-        drop_last=False
+        drop_last=False,
     )
 
 
@@ -153,6 +155,13 @@ def main_train(config):
     start = time.time()
     trainer = Trainer(model, config, dataloader=train_data, optimizer=optimizer)
     trainer.train()
+
+    # Delete all variables to prevent GPU memory leaks
+    del model
+    del optimizer
+    del trainer
+    torch.cuda.empty_cache()
+
     end = time.time()
     total_time = end - start
     logging.debug(f"Training execution time: {total_time} seconds")
@@ -165,6 +174,11 @@ def main_train(config):
     sampler.sample(filename_format="sample_best_{i}.npy")
     logging.info(f"Training completed and best model sampled. You can check log and results in {config.run_name}")
 
+    # Delete all variables to prevent GPU memory leaks
+    del train_data
+    del model
+    del sampler
+    torch.cuda.empty_cache()
 
 def main_sample(config):
     """