ff25e1688bc6636bbfb93ef4045dab456f74732f
cyril-data <ukz42ac@jean-zay1.idris.fr>
2024-02-14 09:43:18 +0100

    good slurm config

--- slurm/start_singularity.sh
+++ slurm/start_singularity.sh
@@ -9,6 +9,12 @@ source .env
 # Echo des commandes lancees
 set -x
 
+# Ensure the logs directory exists or create it if not
+if [ ! -d "$LOG_DIR" ]; then
+  mkdir "$LOG_DIR"
+fi
+
+
 srun singularity exec --nv\
     --bind $SOURCE_DIR:/ddpm\
     --bind $LOG_DIR:/ddpm/logs\
